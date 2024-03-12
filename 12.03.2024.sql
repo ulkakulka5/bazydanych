10b05da6010935ff88a8ab44e58e180ab7366e08
ulkakulka5 <134429167+ulkakulka5@users.noreply.github.com>
2024-03-12 10:23:02 +0100

    Update 12.03.2024.sql

--- 12.03.2024.sql
+++ 12.03.2024.sql
@@ -30,10 +30,38 @@ SET v=v+1
 END WHILE;
 
 
-CREATE FUNCTION potega(liczba int, potega int)
-    -> RETURNS int
-    -> RETURN
-    -> WHILE potega>0 DO
-    -> liczba=liczba * liczba
-    -> potega=potega-1
-    -> END WHILE;
+
+
+
+DELIMITER //
+CREATE FUNCTION potega(liczba INT, potega INT)
+RETURNS INT
+
+    DECLARE x INT;
+    SET x = 1;
+    WHILE potega > 0 DO
+        SET x = x * liczba;
+        SET potega = potega - 1;
+    END WHILE;
+    RETURN x;
+END //
+DELIMITER ;
+
+
+
+
+
+
+CREATE FUNCTION trojkat(a INT, b INT, c INT)
+    -> RETURNS BOOL
+    -> BEGIN
+    -> DECLARE x BOOL;
+    -> IF a+b>c and c+a>b and c+b>a
+    -> THEN SET x=TRUE;
+    -> ELSE
+    -> SET x=FALSE;
+    -> END IF;
+    -> return x;
+    -> END//
+
+
